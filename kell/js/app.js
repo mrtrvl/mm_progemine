b55f06c02037b94335e42aea1856a7b2756b1ae0
Martti Raavel <mrt@mrt.ee>
2018-02-14 11:46:01 +0200

    Seierite animatsioon eraldi funktsiooni, kas jagub 15ga funktsioon

--- kell/js/app.js
+++ kell/js/app.js
@@ -1,16 +1,32 @@
 window.onload = () => {
+    let timeNow;
+    let hourNow;
+    let minuteNow;
+    let secondNow;
+
     tictac();
 };
 
 tictac = () => {
-    let timeNow = new Date();
-    let hourNow = timeNow.getHours();
-    let minuteNow = timeNow.getMinutes();
-    let secondNow = timeNow.getSeconds();
+    timeNow = new Date();
+    hourNow = timeNow.getHours();
+    minuteNow = timeNow.getMinutes();
+    secondNow = timeNow.getSeconds();
 
+    animateClockHands();
+    if (checkIfDividesWithFifteen(secondNow)) {
+        console.log("Divides");
+    }
+
+    requestAnimationFrame(tictac);
+}
+
+animateClockHands = () => {
     document.getElementById("secondHand").style.transform = "rotate(" + secondNow * 6 + "deg)";
     document.getElementById("minuteHand").style.transform = "rotate(" + (minuteNow * 6 + secondNow / 10) + "deg)";
     document.getElementById("hourHand").style.transform = "rotate(" + (hourNow * 30 + minuteNow / 2) + "deg)";
+}
 
-    requestAnimationFrame(tictac);
+checkIfDividesWithFifteen = (dataToDivide) => {
+    return dataToDivide % 15 == 0 ? true : false;
 }
